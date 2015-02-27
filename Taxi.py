44256ae222b0fe2aa8dafa070b36a8815b78d221
Peeranut Chindaononda <PCXD@me.com>
2015-02-27 14:51:59 +0700

    Separate class

---
+++ Taxi.py
@@ -0,0 +1,7 @@
+class Taxi( Transportation ):
+
+   def __init__( self, start, end, distance ):
+      Transportation.__init__( self, start, end, distance)
+
+   def find_cost( self ):
+      return 0
---
+++ Train.py
@@ -0,0 +1,7 @@
+class Train( Transportation ):
+
+   def __init__( self, start, end, distance ):
+      Transportation.__init__( self, start, end, distance)
+
+   def find_cost( self ):
+      return 0
